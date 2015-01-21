a705587c0be63185d00cefa592eea2f969723b90
lakmal <lakmal@rcapl.com>
2015-01-21 10:23:59 +0530

    first commit

---
+++ src/main/java/com/lakmal/RssReader.java
@@ -0,0 +1,12 @@
+package com.lakmal;
+
+public class RssReader {
+
+	public void getRess() {
+		try {
+			System.out.println("in rss reader....");
+		} catch (Exception e) {
+			System.out.println("Error.....");
+		}
+	}
+}
