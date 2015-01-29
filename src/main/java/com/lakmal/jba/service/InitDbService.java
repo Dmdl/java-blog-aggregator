bc38f8078adb7e1370b8181dbc7c26f7c2575cc4
lakmal <lakmal@rcapl.com>
2015-01-29 12:09:08 +0530

    Chanage Db postgress

--- src/main/java/com/lakmal/jba/service/InitDbService.java
+++ src/main/java/com/lakmal/jba/service/InitDbService.java
@@ -1,7 +1,6 @@
 package com.lakmal.jba.service;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lakmal.jba.entity.Blog;
-import com.lakmal.jba.entity.Item;
 import com.lakmal.jba.entity.Role;
 import com.lakmal.jba.entity.User;
 import com.lakmal.jba.repository.BlogRepository;
@@ -36,44 +34,46 @@ public class InitDbService {
 	@Transactional
 	@PostConstruct
 	public void init() {
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		roleRepository.save(roleUser);
+		if (roleRepository.findByName("ROLE_ADMIN") == null) {
+			Role roleUser = new Role();
+			roleUser.setName("ROLE_USER");
+			roleRepository.save(roleUser);
 
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
+			Role roleAdmin = new Role();
+			roleAdmin.setName("ROLE_ADMIN");
+			roleRepository.save(roleAdmin);
 
-		User userAdmin = new User();
-		userAdmin.setName("admin");
-		userAdmin.setEnabled(true);
-		BCryptPasswordEncoder encorder = new BCryptPasswordEncoder();
-		userAdmin.setPassword(encorder.encode("admin"));
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleAdmin);
-		roles.add(roleUser);
-		userAdmin.setRoles(roles);
-		userRepository.save(userAdmin);
+			User userAdmin = new User();
+			userAdmin.setName("admin");
+			userAdmin.setEnabled(true);
+			BCryptPasswordEncoder encorder = new BCryptPasswordEncoder();
+			userAdmin.setPassword(encorder.encode("admin"));
+			List<Role> roles = new ArrayList<Role>();
+			roles.add(roleAdmin);
+			roles.add(roleUser);
+			userAdmin.setRoles(roles);
+			userRepository.save(userAdmin);
 
-		Blog javavids = new Blog();
-		javavids.setName("javavids");
-		javavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
-		javavids.setUser(userAdmin);
-		blogRepository.save(javavids);
+			Blog javavids = new Blog();
+			javavids.setName("javavids");
+			javavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+			javavids.setUser(userAdmin);
+			blogRepository.save(javavids);
 
-		// Item item1 = new Item();
-		// item1.setBlog(javavids);
-		// item1.setTitle("first");
-		// item1.setLink("http://www.javavids.com/");
-		// item1.setPublishedDate(new Date());
-		// itemRepository.save(item1);
-		//
-		// Item item2 = new Item();
-		// item2.setBlog(javavids);
-		// item2.setTitle("second");
-		// item2.setLink("http://www.javavids.com/");
-		// item2.setPublishedDate(new Date());
-		// itemRepository.save(item2);
+			// Item item1 = new Item();
+			// item1.setBlog(javavids);
+			// item1.setTitle("first");
+			// item1.setLink("http://www.javavids.com/");
+			// item1.setPublishedDate(new Date());
+			// itemRepository.save(item1);
+			//
+			// Item item2 = new Item();
+			// item2.setBlog(javavids);
+			// item2.setTitle("second");
+			// item2.setLink("http://www.javavids.com/");
+			// item2.setPublishedDate(new Date());
+			// itemRepository.save(item2);
 
+		}
 	}
 }
